ad8f5c1ad692b294f7a4f3ccb5c5b2d099232cbd
DewaSRY <sdewa6645@gmail.com>
2023-09-01 18:41:12 +0800

    done for now

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,28 +1,49 @@
 import { NextPage } from "next";
 import { AiFillWallet } from "react-icons/ai";
-import { Result } from "@/Util/type";
+import Link from "next/link";
+import WebSearchResults from "@/Component/WebSearchResult";
+interface WebPageProps {
+  searchParams: {
+    searchTerm: string;
+  };
+}
 
-interface WebPageProps {}
-const API_KEY = process.env.API_KEY;
-const CONTEXT_KEY = process.env.CONTEXT_KEY;
-const search = "apple";
-// const url = `https://www.googleapis.com/customsearch/v1?key=AIzaSyDxVK5ivGLtAjAvVB6fpslj8vHRrGn8siY&cx=822bb8f55521f4067&q=lectures`;
-const url = `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${search}`;
-const getData = async () => {
+const url = process.env.url || "";
+const getData = async (search = "lecture") => {
   try {
-    const response = await fetch(url);
+    const response = await fetch(
+      "https://www.googleapis.com/customsearch/v1?key=AIzaSyDxVK5ivGLtAjAvVB6fpslj8vHRrGn8siY&cx=822bb8f55521f4067&q=" +
+        search
+    );
+    if (!response.ok) throw new Error("failed to fetch data");
     const data = await response.json();
-    const result = data.items;
+    const result = data;
+    // console.log(JSON.stringify(data, null, 2));
     return result;
   } catch (err) {
     console.log("test", JSON.stringify(err));
   }
 };
 
-const WebPage: NextPage<WebPageProps> = async () => {
-  const data: Result[] = await getData();
-  console.log(JSON.stringify(data, null, 2));
-  return <div>hallo</div>;
+const WebPage: NextPage<WebPageProps> = async ({ searchParams }) => {
+  const data = await getData(searchParams.searchTerm);
+
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+  return <>{results && <WebSearchResults results={data} />}</>;
 };
 
 export default WebPage;
